11a10e724d142c8fba7097ed421478e2048b1568
sneha Banerje <banerjeesneha090@gmail.com>
2026-07-15 15:18:14 +0530

    adding url module

--- http.js
+++ http.js
@@ -1,19 +1,33 @@
-console.log('YO')
+//buliding a simple http server with node
 const http = require('http');
+
+//creating a log file for logging the request
 const fs = require('fs');
 
+//parsing the req url
+const url = require('url');
+
 const myserver = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        return res.end();
+    }
     //console.log(req);
+    //logging the date for the request
     const log = `${Date.now()}, ${req.url}: new reqquest received\n`;
+
+    const myurl = url.parse(req.url, true);
+    //console.log(myurl);
     fs.appendFile('log.txt', log, (err) => {
         if (err) {
             console.log(err);
         }
-        switch(req.url){
+        //setting the status code and content type
+        switch(myurl.pathname) {
             case '/':
+                const name = myurl.query.name;
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/html');
-                res.end('<h1>Home Page</h1>');
+                res.end(`<h1>Home Page - Hello ${name}</h1>`);
                 break;
             case '/about':
                 res.statusCode = 200;
@@ -28,6 +42,7 @@ const myserver = http.createServer((req, res) => {
         }
     });
 });
+//port 8000
 myserver.listen(8000, () =>{
     console.log('Server is running on port 8000');
 });
